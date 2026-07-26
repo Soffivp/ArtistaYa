484323dbd9494e2353482f3e9ac8b17c2d4b2720
Soffivp <115795322+Soffivp@users.noreply.github.com>
2026-07-26 13:07:49 -0500

    Add files via upload

---
+++ artistas/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('app/', include('app.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
